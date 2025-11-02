e5a3700cefab0a8af66b5d20553a65bfefc3c0f7
Haseeb467 <trickyboy467@gmail.com>
2025-11-02 10:32:55 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Navbar active link switching
+const navLinks = document.querySelectorAll('.nav-links li a');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    navLinks.forEach(l => l.classList.remove('active'));
+    link.classList.add('active');
+  });
+});
